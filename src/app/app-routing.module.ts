e7beb8a59dc8b88880a91f2b830c39e758ba26bc
jeremieL <jeremieL.mail.com>
2018-11-13 16:04:31 +0100

    creation component+routage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ArticleComponent } from './article/article.component';
+import { CommentaireComponent } from './commentaire/commentaire.component';
+import { PersonneComponent } from './personne/personne.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "article", component: ArticleComponent },
+  { path: "commentaire", component: CommentaireComponent },
+  { path: "personne", component: PersonneComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
+import { PersonneComponent } from './personne/personne.component';
+import { CommentaireComponent } from './commentaire/commentaire.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent,
+    PersonneComponent,
+    CommentaireComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
