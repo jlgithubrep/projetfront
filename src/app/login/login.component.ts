e0a0dbf9a89726e4ff402b3c6c9725fc60772e28
jeremieL <jeremieL.mail.com>
2018-11-15 13:22:26 +0100

    login v alpha

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PersonneService } from '../services/personne.services';
+import { Personne } from '../interfaces/Personne';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private personneService: PersonneService) {
+    this.erreur = false;
+  }
+
+  nom: string;
+  mdp: string;
+
+  p: Personne;
+  erreur: boolean;
+
+  login() {
+    this.personneService.login(this.nom, this.mdp).subscribe(res => {
+
+      this.p = <Personne>res;
+      console.log(this.p.nom);
+
+    },
+      err => {
+        console.log(err.status);
+        this.erreur=true;
+      }
+    );
+  }
 
   ngOnInit() {
   }
